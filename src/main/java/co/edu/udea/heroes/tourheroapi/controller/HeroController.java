95c0fbc3d0ce547454c6bd0e4e0fcb07ea8d063f
jlmaring <johny.marin@udea.edu.co>
2022-08-09 21:44:10 -0500

    endpoint ok at http://localhost:8080/heroes/5

--- src/main/java/co/edu/udea/heroes/tourheroapi/controller/HeroController.java
+++ src/main/java/co/edu/udea/heroes/tourheroapi/controller/HeroController.java
@@ -2,12 +2,14 @@ package co.edu.udea.heroes.tourheroapi.controller;
 
 import co.edu.udea.heroes.tourheroapi.model.Hero;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@GetMapping("/heroes")
+@RequestMapping("/heroes")
 public class HeroController {
 
+    @GetMapping("{id}")
     public Hero getHero(Integer id){
         return new Hero(1, "Hulk");
     }
