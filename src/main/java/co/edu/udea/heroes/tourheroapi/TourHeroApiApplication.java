3b6ff912b7d93d0f26cce1d1e9e0e22a50cb6456
jlmaring <johny.marin@udea.edu.co>
2022-08-09 20:56:41 -0500

    first commit whitelabe error ok

---
+++ src/main/java/co/edu/udea/heroes/tourheroapi/TourHeroApiApplication.java
@@ -0,0 +1,13 @@
+package co.edu.udea.heroes.tourheroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourHeroApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourHeroApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/udea/heroes/tourheroapi/TourHeroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.udea.heroes.tourheroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourHeroApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
